8e45f74afec57d99cd9268a347cc8a53eff2d0de
Eric Fray <ericfray@protonmail.com>
2023-07-22 11:20:49 -0400

    added deep linking functionality

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,16 +1,36 @@
-import {NavigationContainer} from '@react-navigation/native';
+import {LinkingOptions, NavigationContainer} from '@react-navigation/native';
 import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import BottomTabNavigator from './BottomTabNavigator';
 import CommentsScreen from '../screens/CommentsScreen/CommentsScreen';
-import { RootNavigatorParamList } from './types';
-
+import {RootNavigatorParamList} from './types';
 
 const Stack = createNativeStackNavigator<RootNavigatorParamList>(); // { Navigator, Screen }
 
+const linking: LinkingOptions<RootNavigatorParamList> = {
+  prefixes: ['notjustphotos://'],
+  config: {
+    initialRouteName: 'Home',
+    screens: {
+      Comments: 'comments',
+      Home: {
+        screens: {
+          HomeStack: {
+            initialRouteName: 'Feed',
+            screens: {
+              UserProfile: 'user/:userId',
+            },
+          },
+        },
+      },
+      //notjustphotos://user/<user_id>
+    },
+  },
+};
+
 const Navigation = () => {
   return (
-    <NavigationContainer>
+    <NavigationContainer linking={linking}>
       <Stack.Navigator
         initialRouteName="Home"
         screenOptions={{headerShown: true}}>
