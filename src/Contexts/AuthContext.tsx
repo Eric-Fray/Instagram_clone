ef9c377b8fd0aef55de8d1631f6f673e34b91612
Eric Fray <ericfray@protonmail.com>
2023-07-25 20:19:46 -0400

    added cognito-cached credentials and signout functionality

--- src/Contexts/AuthContext.tsx
+++ src/Contexts/AuthContext.tsx
@@ -4,9 +4,12 @@ import React, {
   ReactNode,
   SetStateAction,
   useContext,
+  useEffect,
   useState,
 } from 'react';
 import {CognitoUser} from 'amazon-cognito-identity-js';
+import {Auth, Hub} from 'aws-amplify';
+import {HubCallback} from '@aws-amplify/core'
 
 type UserType = CognitoUser | null | undefined;
 type AuthContextType = {
@@ -21,6 +24,31 @@ export const AuthContext = createContext<AuthContextType>({
 
 const AuthContextProvider = ({children}: {children: ReactNode}) => {
   const [user, setUser] = useState<UserType>(undefined);
+
+  useEffect(() => {
+    const checkUser = async () => {
+      try {
+        const authUser = await Auth.currentAuthenticatedUser({
+          bypassCache: true,
+        });
+        setUser(authUser);
+      } catch (e) {
+        setUser(null);
+      }
+    };
+    checkUser();
+  }, []);
+
+  useEffect(() => {
+    const listener: HubCallback = data => {
+      const {event} = data.payload;
+      if (event === 'signOut'){
+        setUser(null);
+      }
+    }
+    const hubCancelToken = Hub.listen('auth', listener)
+    return () => hubCancelToken();
+  }, []);
 
   return (
     <AuthContext.Provider value={{user, setUser}}>
